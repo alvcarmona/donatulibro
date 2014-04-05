c6b16a2109a40000f8a8939d89e99362ad164d1a
Beat <benat@ubuntu.(none)>
2014-04-05 13:08:41 +0200

    Gestion de usuarios corregida.Segunda versión de la vista ayuda.Asignación del  usuario a un libro que añade.

--- app/controllers/libros_controller.rb
+++ app/controllers/libros_controller.rb
@@ -2,6 +2,18 @@ class LibrosController < ApplicationController
 
   hobo_model_controller
 
+
   auto_actions :all
+
+def create
+  @libro = Libro.new(params[:libro])
+  @libro.user_id = current_user.id
+  if @libro.save
+
+    redirect_to @libro
+  else
+    render 'new'
+  end
+end
 
 end
