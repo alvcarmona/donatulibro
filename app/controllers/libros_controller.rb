b4cce642ca9730fa17d42c469fa2d49f70002a7e
Ignacio Huerta <ignacio@ihuerta.net>
2014-04-05 13:10:02 +0200

    Buscar libros

--- app/controllers/libros_controller.rb
+++ app/controllers/libros_controller.rb
@@ -4,4 +4,14 @@ class LibrosController < ApplicationController
 
   auto_actions :all
 
+  def index
+    scope = Libro
+    if params[:autor]
+      scope = Libro.where("autor LIKE ?", "%#{params[:autor]}%").
+                    where("titulo LIKE ?", "%#{params[:titulo]}%").
+                    where("editorial LIKE ?", "%#{params[:editorial]}%")
+    end
+    hobo_index scope
+  end
+
 end
